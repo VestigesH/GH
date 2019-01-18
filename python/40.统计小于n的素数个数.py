f14d19a4fb79636a8f61529548823685db2f7a0a
VestigesH <46252571+VestigesH@users.noreply.github.com>
2019-01-18 23:20:41 +0800

    改错
    首次代码运行结果有误

--- python/40.统计小于n的素数个数.py
+++ python/40.统计小于n的素数个数.py
@@ -1,15 +1,14 @@
+n=int(input("请输入非负整数n:"))
+i=1
+j=2
 counter=0
-num=int(input("请输入非负整数n:" ))
-for i in range(2,num):
-    if num<2:
-        print("0")
-    elif num==2:
-        print("1")
-    elif num%i==0:
-        break
-else:
-    num=num+1
-    counter=counter+1
-print()
+for i in range(1,n):
+    i=i+1
+    for j in range(2,i):
+       if (i%j==0):
+           j=j+1
+           break
+    else:
+        counter=counter+1
 print(counter)
-    
+
