a655a03b190a9b588c2f0711cb5303917f6e80cf
VestigesH <46252571+VestigesH@users.noreply.github.com>
2019-01-18 23:39:24 +0800

    改错
    首次代码输出乘法表的格式错误，1*（2-9）等值出现两次

--- python/8.乘法表.py
+++ python/8.乘法表.py
@@ -1,4 +1,4 @@
 for i in range(1,10):
-    for j in range(1,10):
-        print(i,"*",j,"=",i*j)
-        print()
+    for j in range(1,i+1):
+        print(i,"*",j,"=",i*j,"\t",end=" ")
+    print()
