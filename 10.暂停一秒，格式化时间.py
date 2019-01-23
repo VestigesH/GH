6cf5e1aef92711f9a0d0c4c0c1027451cf6311eb
VestigesH <1541945594@qq.com>
2019-01-23 17:57:16 +0800

    改错

--- 10.暂停一秒，格式化时间.py
+++ 10.暂停一秒，格式化时间.py
@@ -1,7 +1,8 @@
 import datetime
+import time
 hour,mintue,second,microsecond=eval(input("请输入小时,分钟,秒和毫秒:"))
 x=datetime.time(hour,mintue,second,microsecond)
-print(" ")#暂停一秒
+time.sleep(1)#暂停一秒
 #四种方法对时间格式化
 print(x.__format__("%H:%M:%S"))#将时间转化成字符串对象，以指定格式输出
 print(x.strftime("%H:%M:%S"))#方法同上
--- 9.爬楼梯.py
+++ 9.爬楼梯.py
@@ -1,10 +1,12 @@
 # 题目：假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+def getway(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    else:
+        return getway(n-1)+getway(n-2)
+
 n=eval(input("请输入楼梯阶数n:"))
-way=0
-for i in range(0,n+1):#一次一阶
-    for j in range(0,n+1):#一次两阶
-        if (i+2*j==n):
-            way+=1
-print(way,"种不同的方式")
-          
+print(getway(n))
         
