5e43d6663468fff10d4f306b69efde31fe02775f
VestigesH <1541945594@qq.com>
2019-01-28 13:15:02 +0800

    改错

--- python/70..py
+++ python/70..py
@@ -1,14 +1,13 @@
 #题目：编写一个函数，输入n为偶数时，调用函数求1/2+1/4+...+1/n,当输入n为奇数时，调用函数1/1+1/3+...+1/n
 def f(n):
+    s=0
     if n%2==0:
-        s=0
-        for i in range(1,n+1): 
-            s=s+(1/(n*2))
+        for i in range(2,n+2,2): 
+            s=s+(1/i)
         return s
-    if n%2!=0:
-        s=0
-        for j in range(1,n+1):
-            s=s+(1/(n*2-1))
+    else:
+        for i in range(1,n+2,2):
+            s=s+(1/i)
         return s
 
 n=int(input("Input n:"))
