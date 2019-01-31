27953ee45eaebab6ba5777af6d7279086114cbc6
VestigesH <1541945594@qq.com>
2019-01-31 19:15:39 +0800

    改错

--- python/40.小于n的素数个数.py
+++ python/40.小于n的素数个数.py
@@ -1,12 +1,8 @@
 n=int(input("请输入n:"))
-i=1
-j=2
 counter=0
 for i in range(2,n):
-    i+=1
     for j in range(2,i):
        if (i%j==0):
-           j=j+1
            break
     else:
         counter=counter+1
